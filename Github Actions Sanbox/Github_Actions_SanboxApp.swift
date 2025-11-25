ce5f34b4d440bec7e559df1e455b74b872d7f80c
Alexey Novikov <v@2dubs.com>
2025-11-25 09:26:50 -0500

    Initial Commit

---
+++ Github Actions Sanbox/Github_Actions_SanboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Actions_SanboxApp.swift
+//  Github Actions Sanbox
+//
+//  Created by velocityzen on 2025-11-25.
+//
+
+import SwiftUI
+
+@main
+struct Github_Actions_SanboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Github Actions SanboxTests/Github_Actions_SanboxTests.swift
@@ -0,0 +1,16 @@
+//
+//  Github_Actions_SanboxTests.swift
+//  Github Actions SanboxTests
+//
+//  Created by velocityzen on 2025-11-25.
+//
+
+import Testing
+
+struct Github_Actions_SanboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
